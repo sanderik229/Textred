4730c17c25e55bc236b3d44e7428290a61fe3219
sanderik229 <144127176+sanderik229@users.noreply.github.com>
2023-11-12 19:38:20 +0300

    textred

---
+++ Textred/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Textred;
+string[] favoriteColour = new string[] { "Красный", "Розовый", "Оранжевый" };
+
+Human sofia = new Human();
+sofia.Name = "София Алексеевна";
+sofia.Age = 60;
+sofia.MyFavoriteColour = favoriteColour;
+
+Human jenya = new Human();
+jenya.Name = "Евгений";
+jenya.Age = 16;
+jenya.MyFavoriteColour = new string[] { "Темно-зеленый", "Темно-синий" };
+
+Human sasha = new Human();
+sasha.Name = "Александр";
+sasha.Age = 17;
+sasha.MyFavoriteColour = new string[] { "Синий", "Желтый" };
+
+List<Human> humans = new List<Human>();
+humans.Add(jenya);
+humans.Add(sasha);
+humans.Add(sofia);
+
+string json = JsonConvert.SerializeObject(humans);
+File.WriteAllText("Macintosh HD/Users/levonbabaan/Desktop/Result.json", json);
---
+++ Textred/ssss.cs
@@ -0,0 +1,11 @@
+﻿using Textred;
+namespace Textred
+{
+	public class Human
+	{
+		public string Name;
+		public int Age;
+		public string[] MyFavoriteColour;
+	}
+}
+
